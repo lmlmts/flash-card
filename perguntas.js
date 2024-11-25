a7acdfa9954e673c30209f1b8dfb3ed17f6aff90
lmlmts <lemuelmatias722@gmail.com>
2024-11-25 12:13:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Em que ano foi o Mundial do Corinthians?',
+    '2012'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem fez o gol do Mudial?',
+    'Guerreiro'
+)
+
+criaCartao(
+    'Programação',
+    'Contra quem o Corinthians se sagrou campeão do Mundial?',
+    'Contra o Chelsea'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Ao todo quantos mundial o Corinthians tem?',
+    'São 2 Mundiais'
+)
